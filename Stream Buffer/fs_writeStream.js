53e6782924ec9fb7d0a4720530082c0c40bf588f
taib0101 <taibvai13@gmail.com>
2024-09-27 10:35:11 +0600

    updated

--- Stream Buffer/fs_writeStream.js
+++ Stream Buffer/fs_writeStream.js
@@ -3,6 +3,7 @@ let fs = require("fs");
 let readFileStream = fs.createReadStream(`${__dirname}/note.txt`);
 let writeFileStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
+// this is better practice because time and memory complexity
 readFileStream.on("data",(chunk) => {
     writeFileStream.write(chunk);
 })
