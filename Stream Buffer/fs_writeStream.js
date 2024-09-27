df0434b3622ded767197dd256442ea15265afba5
taib0101 <taibvai13@gmail.com>
2024-09-27 10:33:25 +0600

    updated

--- Stream Buffer/fs_writeStream.js
+++ Stream Buffer/fs_writeStream.js
@@ -6,8 +6,8 @@ let writeFileStream = fs.createWriteStream(`${__dirname}/output.txt`);
 readFileStream.on("data",(chunk) => {
     writeFileStream.write(chunk);
 })
-fs.appendFileSync("output.txt","\n")
+fs.appendFileSync(`${__dirname}/output.txt`,"\n")
 
-console.log(fs.readFileSync("output.txt"));
 let buffer = Buffer.from(fs.readFileSync("output.txt"));
 console.log(buffer);
+// console.log(fs.readFileSync("output.txt"));
