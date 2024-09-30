543051ab76aaed9f6e9dfb1196dd4b7ac4fe186c
Mustain Murtaza Taib <65018690+taib0101@users.noreply.github.com>
2024-10-01 01:51:25 +0600

    updated

--- Core Modules/fsModule/readWriteDelete_syncAsync.js
+++ Core Modules/fsModule/readWriteDelete_syncAsync.js
@@ -3,47 +3,52 @@ let fs = require("fs");
 
 // to write on to file
 // synchronous write file
-let writeFile = fs.writeFileSync(`${__dirname}/new.txt`,"bro taib\n");
-let appendFile = fs.appendFileSync(`${__dirname}/new.txt`,"allright man\n");
-
-// // to read from a file
-// synchronous read file
-let readData = fs.readFileSync(`${__dirname}/new.txt`);
-console.log(readData.toString());
-
-// synchronous delete file
-fs.unlinkSync(`${__dirname}/new.txt`);
-
-// -------------------------------------------------------------------------------
+let writeFile = fs.writeFileSync(`${__dirname}/new.txt`,"bro taib\n","utf8");
+// let writeFile = fs.writeFileSync(`${__dirname}/new.txt`,"bro taib\n"); // same
+let appendFile = fs.appendFileSync(`${__dirname}/new.txt`,"allright man\n","utf8");
+// let appendFile = fs.appendFileSync(`${__dirname}/new.txt`,"allright man\n"); // same
 
 
 // asynchronous write file
-// let writeData = new Uint8Array(Buffer.from("Bro fuck off\n"));
-fs.writeFile(`${__dirname}/new.txt`, "Bro fuck off\n", (error) => {
+// fs.writeFile(`${__dirname}/new.txt`, "Bro fuck off\n", (error) => {}); // same
+fs.writeFile(`${__dirname}/new.txt`, "Bro fuck off\n","utf8", (error) => {
     if (error) {
         throw error;
     }
     // console.log("wrote data");
 });
 
-// writeData = new Uint8Array(Buffer.from("fuck off 2\n"));
-fs.appendFile(`${__dirname}/new.txt`, "fuck off 2\n", (error) => {
+// fs.appendFile(`${__dirname}/new.txt`, "fuck off 2\n", (error) => {}); // same
+fs.appendFile(`${__dirname}/new.txt`, "fuck off 2\n","utf8", (error) => {
     if (error) {
         throw error;
     }
     // console.log("wrote data");
 });
+
+// -------------------------------------------------------------------------------
+
+// // to read from a file
+// synchronous read file
+// let readData = fs.readFileSync(`${__dirname}/new.txt`,"utf8");
+let readData = fs.readFileSync(`${__dirname}/new.txt`);
+console.log(readData.toString());
 
 // asynchronous read file
-fs.readFile(`${__dirname}/new.txt`, (error, data) => {
+fs.readFile(`${__dirname}/new.txt`,"utf8", (error, data) => {
     if (error) throw error;
     console.log(data.toString());
 });
 
-// asynchronous delete file
-fs.unlink(`${__dirname}/new.txt`,(error) => {
-    if(error) console.log("delete error");
-    else console.log("file removed");
-});
+// ----------------------------------------------------------------------
+
+// synchronous delete file
+fs.unlinkSync(`${__dirname}/new.txt`);
+
+// // asynchronous delete file
+// fs.unlink(`${__dirname}/new.txt`,(error) => {
+//     if(error) console.log("delete error");
+//     else console.log("file removed");
+// });
 
 // better practice is using asynchronous and more better practice is using async-await 
