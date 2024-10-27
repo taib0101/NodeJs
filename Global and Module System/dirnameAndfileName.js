5cf7f159a45f304a979cdf752f8946b032e3c293
Mustain Murtaza Taib <65018690+taib0101@users.noreply.github.com>
2024-10-27 10:49:59 +0600

    updated

--- Global and Module System/dirnameAndfileName.js
+++ Global and Module System/dirnameAndfileName.js
@@ -1,2 +1,9 @@
+// not for type module 
 console.log("File Path :", __filename);
 console.log("Directory Path :", __dirname);
+
+// for type module
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
